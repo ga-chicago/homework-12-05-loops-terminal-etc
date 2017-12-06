ad1b08550611be989343ba59ce47bbf2b4f4666d
Ryne Laster <ryne.laster@gmail.com>
2017-12-06 09:39:54 -0600

    Homework assignment 1

---
+++ js/app.js
@@ -0,0 +1,39 @@
+console.log("Hello");
+
+// concatenation is a way of adding two strings together;
+// let string = "hello, I am"
+// let stringDos = "Ryne"
+// let myNameIs = string + stringDos
+
+// DRY code stands for "don't repeat yourself". This is important insaving time and writing clean, effeciaent code.
+
+// const a = 4;
+// const b = 53;
+// const c = 57;
+// const d = 16;
+// const e = 'Kevin';
+
+// Section 3
+// inifinite loop: NO
+// infinitie loop II: YES
+// I think it will go through 20 letters of the alphabet
+
+let letters = "A";
+let i = 0
+
+while (i < 20) {
+	letters += "A";
+	i++;
+}
+
+console.log(letters);
+
+// The output is not what I expected as it only copied "A" multiple times
+
+// Section 4
+// For loops allow you to run through a loop x amount of times; whatever is specified. 
+// While loops allow more flexibility with the variables
+
+// let i=0
+// i < 100
+// the console.log()
