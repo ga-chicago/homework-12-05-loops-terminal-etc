ba7ddb55454ec25e239d56d165efb8f6cef531e0
Kait Mikitin <kait.mikitin@gmail.com>
2017-12-05 21:20:03 -0600

    kait completed homework 12/5

---
+++ homework/js/app.js
@@ -0,0 +1,70 @@
+// Hi Reuben! I forget for fork and clone before I started working. 
+// Sorry about that!
+
+// 1. Concatenation is when strings are combined;
+// interpolation is when a declared variable is in a string.
+// 2. DRY code is Don't Repeat Yourself code.
+// It's important to keep code organized and succinct so that 
+// it can be worked on in teams or by someone later.
+
+  // const a = 4;
+  // const b = 53;
+  // const c = 57;
+  // const d = 16;
+  // const e = 'Kevin';
+
+  // a < b
+  // c > d
+  // 'Name' === 'Name'
+  // a < b < c 
+  // a !== a < d
+  // e === 'Kevin'
+  // 48 !== '48'
+
+  // Infinite Loops: Yes, that is an infinite loop, it will always
+  // be "true."
+  // Infinite Loops 2: No, that is not an infinite loop, it runProgram 
+  // switches to being false in the code.
+
+  // I think the concole will log 20 As. 
+
+//   let letters = "A";
+// let i = 0;
+
+// while (i < 20) {
+//  letters += "A";
+//  i++;
+// }
+
+// console.log(letters);
+
+// I was off by one! It logged 21 As.
+
+// for (var i = 0; i <= 999; i++) {
+//  console.log(i);
+// };
+
+// The first part of the control statement is: initialization
+// The second part of the control statement is: condition
+// The third part of the control statement is: increment
+
+// for (var i = 999; i >= 0; i--) {
+//  console.log(i);
+// };
+
+for (var i = 1; i <= 10; i++) {
+  console.log("The value of i is: " + i + " of 10");
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
