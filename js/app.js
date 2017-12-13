9c8ed9c75f780121f3971e00f3ac83749917d7c6
Charles <because@charlesis.me>
2017-12-13 14:19:55 -0600

    This is finally working

---
+++ js/app.js
@@ -0,0 +1,79 @@
+/*
+<!-- Setup -->
+
+1. Concatenation uses 2 stings or intergers where interpolation combines strings/intergers and variables.
+		 Interpolation ex. 
+		 let num = 1
+		 console.log("the number 2 is larger than " + num);
+
+		 Concatenation ex.
+		 let first = "Charles"
+		 let last = "MCGregory"
+		 console.log(first + last);
+
+ 2. "Don't repeat yourself". The path of least resistance is usually is the ideal way of doing things. Loops and classes
+
+<!-- Boolean Expressions -->
+
+ const a = 4;
+ const b = 53;
+ const c = 57;
+ const d = 16;
+ const e = 'Kevin';
+
+ console.log(a < b);
+ console.log(c > d);
+ console.log('Name' === 'Name');
+ console.log(a < b < c);
+ console.log(a <= a < d);
+ console.log(e === 'Kevin');
+ console.log(48 == '48');
+
+<!-- while loops -->
+
+ 1. Yes becuase there is nothing that will make it false...and you told us that it was.
+
+ 2. If there are no parameters to end then it will always run.
+
+ 3. Infinites, AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA endlessly.
+ 
+<!--  Reading code -->
+
+ let letters = "A";
+ let i = 0;
+
+ while(i < 20){
+ 	letters += "A";
+ 	i++;
+ }
+
+ console.log(letters);
+
+<!--  For loops -->
+
+ 1. In a for loop you declare the variable, condition, and expression beforehand instead of inside or as a variable.
+
+<!--  For loop 1 -->
+
+ for(let i = 0; i < 1000; i++){
+ 	console.log(i);
+ }
+
+<!--  for loop control statement -->
+ 
+	 1: initialization
+	 2: condition
+	 3: final-expression
+
+<!--  For loop in reverse -->
+
+ for(let i = 999; i >= 0; i--){
+ 	console.log(i);
+ }
+
+<!--  More counting -->
+
+ for(let i = 1; i <= 10; i++){
+ 	console.log("the value of i is: " + i + " of 10");
+ };
+*/
